35f688671b7a74745b6b55e1d4ef6afa6f1db2c4
Matt Bearman <matt@mattbearman.com>
2023-12-30 20:03:06 +0000

    Change default shape to circle

--- lib/monogram/config.rb
+++ lib/monogram/config.rb
@@ -4,7 +4,7 @@ class Monogram
   class Config
     COLOURS = %w[#B91C1C #B45309 #047857 #1D4ED8 #6D28D9].freeze
     UPPERCASE = true
-    SHAPE = Avatar::SQUARE
+    SHAPE = Avatar::CIRCLE
 
     attr_accessor :colours, :uppercase, :shape
     alias colors= colours=
